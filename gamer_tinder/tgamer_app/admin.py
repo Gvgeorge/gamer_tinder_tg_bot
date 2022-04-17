59179ad18332c515d2f98fb9500a0a4f8815a988
gvgeorge <gvgeorge@mail.ru>
2022-04-17 22:48:04 +0300

    Revert "Revert "initial commit: created models, etc""
    This reverts commit d1de04e44e4413c4c52dff38933afb200094bf2a.

---
+++ gamer_tinder/tgamer_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Game, Player
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
